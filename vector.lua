c8db151b8ac3ce8847b952768eccd80bfb39bcaf
Renan <renanteruoc@gmail.com>
2013-01-29 18:58:35 -0200

    vector stuffs

---
+++ vector.lua
@@ -0,0 +1,43 @@
+
+
+local setmetatable = setmetatable
+
+require("math")
+local math = math
+
+module("vector") do
+
+methods = {}
+
+function methods:rotate(ang)
+    local x = math.cos(ang)*self.x - math.sin(ang)*self.y
+    self.y  = math.sin(ang)*self.x + math.cos(ang)*self.y
+    self.x  = x
+end
+
+function methods:dot(vec)
+    return self.x * vec.x + self.y * vec.y
+end
+
+function methods:scale(scalar)
+    self.x = self.x * scalar
+    self.y = self.y * scalar
+end
+
+function methods:norm2_squared()
+    return self.x * self.x + self.y * self.y
+end
+
+function methods:norm2()
+    return math.sqrt(self:norm2_squared())
+end
+
+meta = { __index = methods }
+
+function meta.new(xval,yval)
+    vec = { x = xval, y = yval }
+    setmetatable(vec,meta)
+    return vec
+end
+
+end -- module
