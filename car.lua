a3d2ea29dfd2e3ff859d38d1dcad4aa5a5f3cc03
Renan T. Carneiro <imano_ob@uspgamedev.org>
2013-03-07 08:13:35 -0300

    Broke up car update into several smaller functions. Code is a lot more self-documenting now.

--- car.lua
+++ car.lua
@@ -23,99 +23,91 @@ local brake = false
 local turn = 0
 
 function load(world)
-    --myball = {}
-    --myball.body = lp.newBody(world,450,450,"dynamic")
-    --myball.shape = lp.newCircleShape(17)
-    --myball.fixture = lp.newFixture(myball.body,myball.shape,2)
     mycar = {}
     mycar.body = lp.newBody(world,450,450,"dynamic")
     mycar.shape = lp.newPolygonShape(10,20,-10,20,-10,-20,10,-20)
     mycar.fixture = lp.newFixture(mycar.body,mycar.shape,3)
-    --joint = lp.newWeldJoint(mycar.body,myball.body,450,450)
-    --mycar.body:setAngularDamping(10)
 end
 
 -- update
 
+local function accelerateCar(frame_front)
+    local scalarAcceleration = 1200
+    local linearAcceleration = vm.new(frame_front.x, frame_front.y)
+    --TODO: Arrumar nomes de variaveis para a linha abaixo fazer mais sentido
+    linearAcceleration:scale(scalarAcceleration*accelerate)
+    mycar.body:applyForce(linearAcceleration.x, linearAcceleration.y)
+end
+
+local function turnCar(vel_front)
+   local v = vel_front:norm2_squared()
+   v = math.min(v/2500, 1)
+   mycar.body:applyTorque(turn*6000*v)
+end
+
+local function brakeCar(vel_front)
+    local brakeForceScale = -2
+    local brake_force = vm.new(vel_front.x, vel_front.y)
+    brake_force:scale(brakeForceScale)
+    mycar.body:applyForce(brake_force.x,brake_force.y)
+ end
+
+local function killLateralMomentum(vel_lateral)
+    local lateral_kill_impulse = vm.new(vel_lateral.x, vel_lateral.y)
+    lateral_kill_impulse:scale(-1 * mycar.body:getMass())
+    local v = lateral_kill_impulse:norm2_squared()
+    if v > 100 then
+        lateral_kill_impulse:scale( 10 / math.sqrt(v))
+    end
+    mycar.body:applyLinearImpulse(lateral_kill_impulse.x,lateral_kill_impulse.y)
+end
+
+local function reduceForwardMomentum(vel_front)
+    local frictionScale = -0.8
+    local forward_friction = vm.new(vel_front.x, vel_front.y)
+    forward_friction:scale(frictionScale * mycar.body:getMass() )
+    mycar.body:applyForce(forward_friction.x, forward_friction.y)
+end
+
+local function reduceAngularMomentum()
+    local reductionScale = -0.5
+    mycar.body:applyAngularImpulse( reductionScale * mycar.body:getInertia() * mycar.body:getAngularVelocity() )
+end
+
 function update()
     -- know the car
    
-    local nright = { x = 1 , y = 0 }
-    local nup = { x = 0 , y = -1 }
-    setmetatable(nright,vm)
-    setmetatable(nup,vm)
-
-    local vel_linear = { x = 0, y = 0 }
-    setmetatable(vel_linear,vm)
+    local nright = vm.new(1,0)
+    local nup = vm.new(0,-1)
+    
+    local vel_linear = vm.new(0,0)
     vel_linear.x, vel_linear.y = mycar.body:getLinearVelocity()
 
-    local vel_front = { x = 0, y = 0 }
-    local vel_lateral = { x = 0, y = 0}
-    setmetatable(vel_front,vm)
-    setmetatable(vel_lateral,vm)
+    local frame_front = vm.new(0,0)
+    local frame_lateral = vm.new(0,0)
 
-    vel_front.x, vel_front.y = mycar.body:getWorldVector(nup.x,nup.y)
-    vel_lateral.x, vel_lateral.y = mycar.body:getWorldVector(nright.x,nright.y)
+    frame_front.x, frame_front.y = mycar.body:getWorldVector(nup.x,nup.y)
+    frame_lateral.x, frame_lateral.y = mycar.body:getWorldVector(nright.x,nright.y)
 
-    local frame_front = vm.new(vel_front.x, vel_front.y)
-    local frame_lateral = vm.new(vel_lateral.x, vel_lateral.y)
+    local vel_front = vm.new(frame_front.x, frame_front.y)
+    local vel_lateral = vm.new(frame_lateral.x, frame_lateral.y)
 
     vel_front:scale(vel_front:dot(vel_linear))
     vel_lateral:scale(vel_lateral:dot(vel_linear))
 
 
-    -- move the car
-
     if accelerate ~= 0 then
-        local accel = vm.new(frame_front.x, frame_front.y)
-        accel:scale(1200*accelerate)
-        mycar.body:applyForce(accel.x, accel.y)
+        accelerateCar(frame_front)
     end
-
-
-    -- turn the car
-
-    local v = vel_front:norm2_squared()
-    if v <= 2500 then
-        v = v/2500
-    else
-        v = 1
-    end
-
     if turn ~= 0 then
-        mycar.body:applyTorque(turn*6000*v)
+        turnCar(vel_front)
     end
-
-
-    -- brake the car
-
     if brake then
-       local brake_force = vm.new(vel_front.x, vel_front.y)
-       brake_force:scale(-2)
-       mycar.body:applyForce(brake_force.x,brake_force.y)
+        brakeCar(vel_front)
     end
-
-    -- kill lateral momentum
-
-    local lateral_kill_impulse = vel_lateral
-    setmetatable(lateral_kill_impulse,vm)
-    lateral_kill_impulse:scale(-1 * mycar.body:getMass())
-    v = lateral_kill_impulse:norm2_squared()
-    if v > 100 then
-        lateral_kill_impulse:scale( 10 / math.sqrt(v))
-    end
-    mycar.body:applyLinearImpulse(lateral_kill_impulse.x,lateral_kill_impulse.y)
-
-    -- reduce forward momentum
-
-    local forward_friction = vm.new(vel_front.x, vel_front.y)
-    forward_friction:scale(-0.8 * mycar.body:getMass() )
-    mycar.body:applyForce(forward_friction.x, forward_friction.y)
-
-    -- reduce angular momentum
-    mycar.body:applyAngularImpulse( -0.5 * mycar.body:getInertia() * mycar.body:getAngularVelocity() )
-    
-
+    killLateralMomentum(vel_lateral)
+    reduceForwardMomentum(vel_front)  
+    reduceAngularMomentum()
 end
 
 -- input
